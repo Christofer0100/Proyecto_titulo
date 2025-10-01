5eb5fb3e7fe8b327ea9366b617d5624bbb7b45e7
eduardopozog10 <eduardopozog6@gmail.com>
2025-10-01 15:15:00 -0300

    Cambio modelo predictivo

--- Api_Modelo_Predictivo/Readme.txt
+++ Api_Modelo_Predictivo/Readme.txt
@@ -19,6 +19,8 @@
    - Swagger UI: http://127.0.0.1:8000/docs
    - Redoc:      http://127.0.0.1:8000/redoc
 
+Opcional: 
+rmdir .venv -Recurse -Force (Eliminar entorno virtual)
 
 
 
--- Api_Modelo_Predictivo/backend/.venv/Scripts/activate.bat
+++ Api_Modelo_Predictivo/backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Eduardo Pozo\Desktop\atp-chile-open-web\backend\.venv
+set VIRTUAL_ENV=C:\Users\Eduardo Pozo\Documents\GitHub\Proyecto_titulo\Api_Modelo_Predictivo\backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
